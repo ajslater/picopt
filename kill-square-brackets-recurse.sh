1d9969f249674067775990f90debf282e2827b49
AJ Slater <aj@slater.net>
2010-04-22 23:11:43 -0700

    remove cruft. move more things into linux only and convert.

--- kill-square-brackets-recurse.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in $*; do
-    cd $i
-    find . -name "*\[*" -print0 | xargs -0 kill-square-brackets.sh
-done;
--- kill-square-brackets.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IFS=$'\n'
-for i in $*; do 
-  filtered=`echo "$i" | sed 's/\[/(/' | sed 's/\]/)/'`
-  if [ "$filtered" != $i ]; then
-      echo mv "$i" "$filtered"
-      mv "$i" "$filtered"
-  fi
-done;
