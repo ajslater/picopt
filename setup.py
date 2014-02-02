7e169d62bfb60eeb105e9d40b2a5932d63881460
AJ Slater <aj@slater.net>
2014-02-01 23:39:22 -0800

    enhance setup.py. remove ez_setup stuff

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
+""" Setup file for Picopt
+Reference:
+https://hynek.me/articles/sharing-your-labor-of-love-pypi-quick-and-dirty/
+"""
 from distutils.core import setup
-from ez_setup import use_setuptools
+#from ez_setup import use_setuptools
 from pip.req import parse_requirements
 from picopt import __version__
 
-use_setuptools()
+#use_setuptools()
 
-install_reqs=parse_requirements('requirements.txt')
-req_list=[str(ir.req) for ir in install_reqs]
+INSTALL_REQS = parse_requirements('requirements.txt')
+REQ_LIST = [str(ir.req) for ir in INSTALL_REQS]
 
 setup(
     name='picopt',
@@ -14,9 +18,26 @@ setup(
     description='Optimize image files and comic archives with external tools',
     author='AJ Slater',
     author_email='aj@slater.net',
-    url='http://github.com/ajslater/picopt/',
+    url='https://github.com/ajslater/picopt/',
     py_modules=['picopt'],
-    install_requires=req_list,
+    install_requires=REQ_LIST,
     scripts=['picopt.py'],
     long_description=open('README.md').read(),
+    license="GPL v2",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Environment :: Console',
+        'License :: OSI Approved :: GPL v2',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+        'Topic :: Multimedia :: Graphics :: Graphics Conversion'
+    ]
 )
