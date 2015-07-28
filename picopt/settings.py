c27009a4d445208d087cdfb799db49a5bdd62e53
AJ Slater <aj@slater.net>
2015-07-27 22:52:11 -0700

    alphabetize settings.py

--- picopt/settings.py
+++ picopt/settings.py
@@ -2,36 +2,32 @@ import os
 import multiprocessing
 
 
-__version__ = 2.0
-
-
 class Settings(object):
-    recurse = False
-    verbose = 1
     advpng = False
-    comics = False
-    formats = set()
-    optipng = True
-    pngout = True
-    mozjpeg = True
-    jpegrescan = True
-    jpegtran_prog = True
-    jpegtran = True
-    gifsicle = True
-    to_png_formats = set()
-    follow_symlinks = True
-    dir = os.getcwd()
-    bigger = False
-    record_timestamp = False
-    optimize_after = None
-    test = False
-    list_only = False
-    version = __version__
-    destroy_metadata = False
-    jobs = multiprocessing.cpu_count()
-    paths = set()
     archive_name = None
+    bigger = False
+    comics = False
+    destroy_metadata = False
+    dir = os.getcwd()
+    follow_symlinks = True
+    formats = set()
+    gifsicle = True
+    jobs = multiprocessing.cpu_count()
+    jpegrescan = True
     jpegrescan_mutithread = False
+    jpegtran = True
+    jpegtran_prog = True
+    list_only = False
+    mozjpeg = True
+    optimize_after = None
+    optipng = True
+    paths = set()
+    pngout = True
+    record_timestamp = False
+    recurse = False
+    test = False
+    to_png_formats = set()
+    verbose = 1
 
     @classmethod
     def apply(cls, settings):
