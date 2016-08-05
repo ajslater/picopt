738d4d1f09918780c82ab8855006a09e7064cdc5
AJ Slater <aj@slater.net>
2016-08-05 12:58:14 -0700

    use nosetests to run the old tests

---
+++ tests/cli/test_images_dir.py
@@ -0,0 +1,36 @@
+"""Test comic format."""
+import os
+import glob
+import shutil
+from unittest import TestCase
+
+from picopt import cli
+
+TEST_FILES_SRC = 'tests/test_files'
+TEST_FILES_DST_ROOT = '/tmp'
+TEST_FILES_DST = '%s/%s' % (TEST_FILES_DST_ROOT, 'picopt_tests')
+
+
+class TestCLI(TestCase):
+
+    def setUp(self):
+        if os.path.exists(TEST_FILES_DST):
+            shutil.rmtree(TEST_FILES_DST)
+        shutil.copytree(TEST_FILES_SRC, TEST_FILES_DST)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_FILES_DST)
+
+
+class TestCLIImages(TestCLI):
+
+    def test_walk_images(self):
+        args = [None] +  glob.glob(TEST_FILES_DST+'/*')
+        cli.main(args)
+
+class TestCLIEverything(TestCLI):
+
+    def test_all_twice(self):
+        args = [None, '-rct', TEST_FILES_DST]
+        cli.main(args)
+        cli.main(args)
