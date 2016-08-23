5668a006d507c93871e12694f6a7c00b506ecf50
AJ Slater <aj@slater.net>
2016-08-22 23:09:34 -0700

    small comic test

---
+++ tests/cli/test_small_comic.py
@@ -0,0 +1,28 @@
+"""Test comic format."""
+import os
+import shutil
+from unittest import TestCase
+
+from picopt import cli
+
+TEST_FILES_SRC = 'tests/test_files'
+TEST_FILES_DST_ROOT = '/tmp'
+TEST_FILES_DST = '%s/%s' % (TEST_FILES_DST_ROOT, 'picopt_tests')
+TEST_SMALL_COMIC = "%s/comic_archives/test_small_cbz.cbz" % TEST_FILES_DST
+
+
+class TestCLI(TestCase):
+
+    def setUp(self):
+        if os.path.exists(TEST_FILES_DST):
+            shutil.rmtree(TEST_FILES_DST)
+        shutil.copytree(TEST_FILES_SRC, TEST_FILES_DST)
+
+    def tearDown(self):
+        shutil.rmtree(TEST_FILES_DST)
+
+class TestCLISmallComic(TestCLI):
+
+    def test_small_comic(self):
+        args = [None, '-rct', TEST_SMALL_COMIC]
+        cli.main(args)
