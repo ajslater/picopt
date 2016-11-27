8591ac5619b0ae3f6d9ec770fd37587ef8771b4c
AJ Slater <aj@slater.net>
2016-11-26 16:50:34 -0800

    simplify and correct timestamp and non file checks in walk_file

--- picopt/walk.py
+++ picopt/walk.py
@@ -14,6 +14,7 @@ from .settings import Settings
 def _comic_archive_skip(report_stats):
     return report_stats
 
+
 def walk_comic_archive(filename_full, image_format, optimize_after):
     """Optimize a comic archive."""
     # uncompress archive
@@ -39,55 +40,55 @@ def walk_comic_archive(filename_full, image_format, optimize_after):
                                      args=(args,))
 
 
-def _process_if_not_file(filename_full, walk_after, recurse,
-                         archive_mtime):
+def _is_skippable(filename_full):
     """Handle things that are not optimizable files."""
-    result_set = set()
 
     # File types
     if not Settings.follow_symlinks and os.path.islink(filename_full):
-        return result_set
+        return True
     elif os.path.basename(filename_full) == timestamp.RECORD_FILENAME:
-        return result_set
-    elif os.path.isdir(filename_full):
-        results = walk_dir(filename_full, walk_after, recurse,
-                           archive_mtime)
-        result_set = result_set.union(results)
-        return result_set
+        return True
     elif not os.path.exists(filename_full):
         if Settings.verbose:
             print(filename_full, 'was not found.')
-        return result_set
+        return True
 
-    # Timestamp
-    if walk_after is not None:
-        mtime = os.stat(filename_full).st_mtime
-        # if the file is in an archive, use the archive time if it
-        # is newer. This helps if you have a new archive that you
-        # collected from someone who put really old files in it that
-        # should still be optimised
-        if archive_mtime is not None:
-            mtime = max(mtime, archive_mtime)
-        if mtime <= walk_after:
-            return result_set
-
-    return result_set
+    return False
 
 
-def walk_file(filename, walk_after, recurse=None,
-              archive_mtime=None):
+def _is_older_than_timestamp(filename, walk_after, archive_mtime):
+    if walk_after is None:
+        return False
+
+    mtime = os.stat(filename).st_mtime
+    # if the file is in an archive, use the archive time if it
+    # is newer. This helps if you have a new archive that you
+    # collected from someone who put really old files in it that
+    # should still be optimised
+    if archive_mtime is not None:
+        mtime = max(mtime, archive_mtime)
+    return mtime <= walk_after
+
+
+def walk_file(filename, walk_after, recurse=None, archive_mtime=None):
     """Optimize an individual file."""
     filename = os.path.normpath(filename)
 
-    walk_after = timestamp.get_walk_after(filename, walk_after)
-    result_set = _process_if_not_file(
-        filename, walk_after, recurse, archive_mtime)
-    if len(result_set):
-        return result_set
-
     result_set = set()
 
-    # Image format
+    if _is_skippable(filename):
+        return result_set
+
+    walk_after = timestamp.get_walk_after(filename, walk_after)
+
+    # File is a directory
+    if os.path.isdir(filename):
+        return walk_dir(filename, walk_after, recurse, archive_mtime)
+
+    if _is_older_than_timestamp(filename, walk_after, archive_mtime):
+        return result_set
+
+    # Check image format
     image_format = detect_format.detect_file(filename)
     if not image_format:
         return result_set
