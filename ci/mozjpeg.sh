82807c6552fa96b973c866574aa184da0b422421
AJ Slater <aj@slater.net>
2017-11-24 19:59:52 -0800

    hack for bad circleci paths

--- ci/mozjpeg.sh
+++ ci/mozjpeg.sh
@@ -3,4 +3,4 @@
 cd /tmp
 wget https://mozjpeg.codelove.de/bin/mozjpeg_3.1_amd64.deb
 sudo dpkg -i mozjpeg_3.1_amd64.deb
-sudo ln -sf /opt/mozjpeg/bin/jpegtran /usr/local/bin/mozjpeg
+sudo ln -sf /opt/mozjpeg/bin/jpegtran /usr/bin/mozjpeg
--- ci/pngout.sh
+++ ci/pngout.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # pngout
-curl -v http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux-static.tar.gz | tar xz | sudo tee /usr/local/bin/pngout
+curl -v http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux-static.tar.gz | tar xz | sudo tee /usr/bin/pngout
