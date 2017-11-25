284b18a1a7ad9110e9dc0cfec220b70ba2fa0036
AJ Slater <aj@slater.net>
2017-11-24 20:21:53 -0800

    restore sudo

--- ci/pngout.sh
+++ ci/pngout.sh
@@ -6,8 +6,9 @@ URL=http://static.jonof.id.au/dl/kenutils/pngout-20150319-linux-static.tar.gz
 TARBALL=/tmp/pngout.tar.gz
 CONTENTS=/tmp/pngout-20150319-linux-static
 
+cd /tmp/
 curl -o "$TARBALL" "$URL"
 
 tar xzf "$TARBALL"
-mv "$CONTENTS/x86_64/pngout-static" /usr/local/bin/pngout
+sudo mv "$CONTENTS/x86_64/pngout-static" /usr/local/bin/pngout
 rm -rf "$TARBALL" "$CONTENTS"
