b103ea72a18dbda9da851086bd32c248ba62ff83
AJ Slater <aj@slater.net>
2018-07-10 15:30:37 -0700

    fix for requirments parsing"

--- setup.py
+++ setup.py
@@ -10,9 +10,11 @@ import os
 import re
 import sys
 
-try: # for pip >= 10
+try:
+    # for pip >= 10
     from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
+except ImportError:
+    # for pip <= 9.0.3
     from pip.req import parse_requirements
 from setuptools import find_packages, setup
 
