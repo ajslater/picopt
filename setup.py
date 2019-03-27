261da837027563c1dc3ed07b70e1086520a60402
AJ Slater <aj@slater.net>
2019-03-26 17:52:57 -0700

    isort

--- setup.py
+++ setup.py
@@ -10,13 +10,14 @@ import os
 import re
 import sys
 
+from setuptools import find_packages, setup
+
 try:
     # for pip >= 10
     from pip._internal.req import parse_requirements
 except ImportError:
     # for pip <= 9.0.3
     from pip.req import parse_requirements
-from setuptools import find_packages, setup
 
 README_FILENAME = "README.rst"
 REQUIREMENTS = {
