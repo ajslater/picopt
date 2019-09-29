0088ee84680492a93927314f69a6aa1901120a01
AJ Slater <aj@slater.net>
2019-09-30 00:59:44 +0545

    remove verbosity

--- bin/tests.sh
+++ bin/tests.sh
@@ -2,16 +2,12 @@
 set -xeuo pipefail
 mkdir -p /tmp/test-results/nose
 nosetests-2.7 \
-    -v \
-    --nocapture \
     --with-coverage \
     --cover-package=picopt \
     --with-xunit \
     --xunit-file=/tmp/test-results/nose/noseresults-2.7.xml
 rm -f .coverage
 nosetests-3.4 \
-    -v \
-    --nocapture \
     --with-coverage \
     --cover-package=picopt \
     --with-xunit \
