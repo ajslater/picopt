c236808aa718f7787c286bf0196f5c953ce3b973
AJ Slater <aj@slater.net>
2019-09-29 21:44:50 +0545

    actually read the bytes

--- picopt/formats/png_bit_depth.py
+++ picopt/formats/png_bit_depth.py
@@ -9,14 +9,14 @@ PNG_HEADER = ('\x89', 'P', 'N', 'G', '\r', '\n', '\x1a', '\n')
 def png_bit_depth(filename):
     with open(filename, 'rb') as img:
         img.seek(0)
-        header = struct.unpack('cccccccc', img)
+        header = struct.unpack('cccccccc', img.read('8'))
         # header = tuple(img.read(8))
         if header != PNG_HEADER:
             print('not a png!')
             return
 
         img.seek(24)  # bit depth offset
-        depth = ord(struct.unpack('c', img)[0])
+        depth = ord(struct.unpack('c', img.read('1'))[0])
         return depth
         #return tuple(img.read(1))[0]
 
