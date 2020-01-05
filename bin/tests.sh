ae649d59e2c658f7780c88a5f553c0c888039e9a
AJ Slater <aj@slater.net>
2020-01-05 14:05:23 -0800

    try to get test-results to store again. remove unused xml output

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 mkdir -p test-results
-poetry run coverage run -m pytest --junitxml=test-results/junit.xml
+poetry run coverage run -m pytest --junitxml=test-results/pytest/results.xml
 poetry run coverage combine
 poetry run coverage report
-poetry run coverage xml
 poetry run coverage html
 poetry run mypy picopt tests
