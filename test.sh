0cca533b2771437ee9e3dad890397d908c733908
AJ Slater <aj@slater.net>
2020-01-07 17:49:08 -0800

    helpful comments

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 set -euxo pipefail
 mkdir -p test-results
 poetry run pytest
-poetry run coverage erase # py-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
+# pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
+poetry run coverage erase
