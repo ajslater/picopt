bb4ed7ca27eb109613a29bf26d4ee8cb564f32bb
AJ Slater <aj@slater.net>
2020-01-07 16:43:42 -0800

    the new way of doing things

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
-poetry run black --fast --check .
 mkdir -p test-results
-poetry run coverage run -m pytest --flake8 --isort -rs --junitxml=test-results/pytest/results.xml
+poetry run pytest
 # poetry run coverage combine
-poetry run coverage report
-poetry run coverage html
+#poetry run coverage report
+#poetry run coverage html
 poetry run mypy picopt tests
