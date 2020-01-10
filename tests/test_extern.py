e32b349f4c8e531171785e69663619ede3742829
AJ Slater <aj@slater.net>
2020-01-09 20:05:01 -0800

    100% test coverage for extern module

--- tests/test_extern.py
+++ tests/test_extern.py
@@ -1,4 +1,5 @@
 """Test running external commands."""
+from subprocess import CalledProcessError
 from unittest import TestCase
 
 from picopt import extern
@@ -7,16 +8,31 @@ from picopt import extern
 __all__ = ()  # hides module from pydocstring
 
 
+def test_extargs_class():
+    eac = extern.ExtArgs("a", "b")
+    assert type(eac) == extern.ExtArgs
+
+
 class TestDoesExernalProgramRun(TestCase):
     def test_true(self) -> None:
-        res = extern.does_external_program_run("true", True)
+        res = extern.does_external_program_run("true", 1)
         self.assertTrue(res)
 
     def test_garbage(self) -> None:
-        res = extern.does_external_program_run("asdkfjadskl", True)
+        res = extern.does_external_program_run("asdkfjadskl", 1)
+        self.assertFalse(res)
+
+    def test_garbage_verbose(self) -> None:
+        res = extern.does_external_program_run("asdkfjadskl", 2)
         self.assertFalse(res)
 
 
 class TestRunExt(TestCase):
     def test_true(self) -> None:
         extern.run_ext(("true",))
+
+    def test_bad(self) -> None:
+        try:
+            extern.run_ext(("false",))
+        except CalledProcessError:
+            pass
