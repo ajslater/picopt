302980d1ca467bf6ca36d6bb6df8ee05d71547fd
AJ Slater <aj@slater.net>
2020-01-11 17:23:58 -0800

    good tests for timestamp but fail on timing issue

--- picopt/timestamp.py
+++ picopt/timestamp.py
@@ -11,6 +11,10 @@ from .settings import Settings
 
 
 RECORD_FILENAME = f".{PROGRAM_NAME}_timestamp"
+_REASON_DEFAULT = ""
+_REASON_SYMLINK = "Not setting timestamp because not following symlinks"
+_REASON_NONDIR = "Not setting timestamp for a non-directory"
+# TODO These should not be module global
 TIMESTAMP_CACHE: Dict[Path, Optional[float]] = {}
 OLD_TIMESTAMPS: Set[Path] = set()
 
@@ -23,7 +27,7 @@ def _get_timestamp(
 
     Optionally mark it for removal if we're going to write another one.
     """
-    record_path = Path(dirname_full).joinpath(RECORD_FILENAME)
+    record_path = dirname_full / RECORD_FILENAME
 
     if not record_path.exists():
         return None
@@ -103,32 +107,77 @@ def get_walk_after(
     return _max_timestamps(settings, dirname, True, optimize_after)
 
 
-def record_timestamp(settings: Settings, full_path: Path) -> None:
-    """Record the timestamp of running in a dotfile."""
+def _should_record_timestamp(settings: Settings, full_path: Path) -> Tuple[bool, str]:
+    """Determine if we should we record a timestamp at all."""
+    record = True
+    reason = _REASON_DEFAULT
     if settings.test or settings.list_only or not settings.record_timestamp:
-        return
-    if not settings.follow_symlinks and full_path.is_symlink():
-        if settings.verbose:
-            print("Not setting timestamp because not following symlinks")
-        return
-    if not full_path.is_dir():
-        if settings.verbose:
-            print("Not setting timestamp for a non-directory")
-        return
+        record = False
+    elif not settings.follow_symlinks and full_path.is_symlink():
+        record = False
+        reason = _REASON_SYMLINK
+    elif not full_path.is_dir():
+        record = False
+        reason = _REASON_NONDIR
+    return record, reason
 
-    record_filepath = full_path.joinpath(RECORD_FILENAME)
+
+def _remove_old_timestamps(
+    settings: Settings, full_path: Path, record_filepath: Path
+) -> Dict[Path, Optional[OSError]]:
+    """Remove old timestamps after setting a new one."""
+    removed: Dict[Path, Optional[OSError]] = {}
+    for path in OLD_TIMESTAMPS:
+        # only remove timestamps below the curent path
+        # but don't remove the timestamp we just set!
+        if (
+            not path.exists()
+            or full_path not in path.parents
+            or path.samefile(record_filepath)
+        ):
+            continue
+        try:
+            path.unlink()
+            removed[path] = None
+        except OSError as err:
+            removed[path] = err
+
+    if not settings.verbose:
+        return removed
+
+    for path, error in removed.items():
+        if error is None:
+            print(f"Removed old timestamp: {path}")
+        else:
+            print(f"Could not remove old timestamp: {path}: {error.strerror}")
+
+    return removed
+
+
+def _record_timestamp(settings: Settings, full_path: Path) -> Optional[Path]:
+    """Record the timestamp utilitiy without extra actios."""
+    record_filepath = full_path / RECORD_FILENAME
     try:
         record_filepath.touch()
         if settings.verbose:
             print(f"Set timestamp: {record_filepath}")
-        for path in OLD_TIMESTAMPS:
-            if str(path).startswith(str(full_path)) and not path.samefile(
-                record_filepath
-            ):
-                # only remove timestamps below the curent path
-                # but don't remove the timestamp we just set!
-                path.unlink()
-                if settings.verbose:
-                    print(f"Removed old timestamp: {path}")
-    except IOError:
-        print(f"Could not set timestamp in {full_path}")
+    except OSError as err:
+        print(f"Could not set timestamp in {full_path}: {err.strerror}")
+        return None
+    return record_filepath
+
+
+def record_timestamp(settings: Settings, full_path: Path) -> None:
+    """Record the timestamp of running in a dotfile."""
+    record, reason = _should_record_timestamp(settings, full_path)
+    if not record:
+        if settings.verbose:
+            print(reason)
+        return
+
+    record_filepath = _record_timestamp(settings, full_path)
+    print("filepath", record_filepath)
+    if record_filepath is None:
+        return
+
+    _remove_old_timestamps(settings, full_path, record_filepath)
--- tests/test_timestamp.py
+++ tests/test_timestamp.py
@@ -1,42 +1,303 @@
 """Test timestamp module."""
+import fcntl
+import shutil
+
 from pathlib import Path
 from typing import Tuple
-from unittest import TestCase
 
 from picopt import timestamp
 from picopt.settings import Settings
 
 
 __all__ = ()
+TMP_ROOT = Path("/tmp/picopt-timestamp")
+DEEP_TMP = TMP_ROOT / "deep"
+DEEP_TMP_FILE = TMP_ROOT / "deep/file"
 TEST_FILES_ROOT = Path("tests/test_files")
-COMIC_ROOT = TEST_FILES_ROOT.joinpath("comic_archives")
-SETTINGS = Settings()
 
 
-def _get_timestamp_setup() -> Tuple[Path, float]:
-    record_path = TEST_FILES_ROOT.joinpath(timestamp.RECORD_FILENAME)
+def _setup(path: Path = TMP_ROOT) -> Tuple[Path, float, Settings]:
+    _teardown()
+    path.mkdir(parents=True)
+    record_path = path / timestamp.RECORD_FILENAME
     record_path.touch()
-    return record_path, record_path.stat().st_mtime
+    return record_path, record_path.stat().st_mtime, Settings()
 
 
-class TestTimestamp(TestCase):
-    def test_get_timestamp_ne(self) -> None:
-        path = Path(TEST_FILES_ROOT).joinpath("BLARGH")
-        res = timestamp._get_timestamp(SETTINGS, path, False)
-        self.assertIsNone(res)
+def _setup_record() -> Tuple[Path, Settings]:
+    _teardown()
+    record_path = TMP_ROOT / timestamp.RECORD_FILENAME
+    DEEP_TMP.mkdir(parents=True)
+    return record_path, Settings()
 
-    def test_get_timestamp_no_remove(self) -> None:
-        record_filename, mtime = _get_timestamp_setup()
-        res = timestamp._get_timestamp(SETTINGS, TEST_FILES_ROOT, False)
 
-        self.assertEqual(res, mtime)
-        self.assertTrue(Path(record_filename).exists())
+def _teardown():
+    if TMP_ROOT.exists():
+        shutil.rmtree(TMP_ROOT)
 
-    def test_get_timestamp_remove(self) -> None:
-        record_filename, mtime = _get_timestamp_setup()
 
-        Settings.record_timestamp = True
-        res = timestamp._get_timestamp(SETTINGS, TEST_FILES_ROOT, True)
+def test_get_timestamp_invalid() -> None:
+    path = TMP_ROOT / "BLARGH"
+    res = timestamp._get_timestamp(Settings(), path, False)
+    assert res is None
 
-        self.assertEqual(res, mtime)
-        self.assertIn(record_filename, timestamp.OLD_TIMESTAMPS)
+
+def test_get_timestamp_no_remove() -> None:
+    record_path, mtime, settings = _setup()
+    res = timestamp._get_timestamp(settings, TMP_ROOT, False)
+
+    assert res == mtime
+    _teardown()
+
+
+def test_get_timestamp_remove() -> None:
+    record_path, mtime, settings = _setup()
+    settings.record_timestamp = True
+    res = timestamp._get_timestamp(settings, TMP_ROOT, True)
+
+    assert res == mtime
+    assert record_path in timestamp.OLD_TIMESTAMPS
+    _teardown()
+
+
+def test_get_timestamp_cached():
+    record_path, mtime, settings = _setup()
+    assert TMP_ROOT not in timestamp.TIMESTAMP_CACHE
+    res = timestamp._get_timestamp_cached(settings, TMP_ROOT, False)
+    assert res == mtime
+    assert timestamp.TIMESTAMP_CACHE[TMP_ROOT] == mtime
+
+    res = timestamp._get_timestamp_cached(settings, TMP_ROOT, False)
+    assert res == mtime
+    _teardown()
+
+
+def test_max_none_nums():
+    res = timestamp.max_none((1, 2))
+    assert res == 2
+
+
+def test_max_none_none():
+    res = timestamp.max_none((1, None))
+    assert res == 1
+
+
+def test_max_none_none_none():
+    res = timestamp.max_none((None, None))
+    assert res is None
+
+
+def test_max_timestamps_none():
+    record_path, mtime, settings = _setup()
+    res = timestamp._max_timestamps(settings, TMP_ROOT, False, None)
+    assert res == mtime
+    _teardown()
+
+
+def test_max_timestamps_tstamp():
+    record_path, mtime, settings = _setup()
+    tstamp = mtime + 100
+    res = timestamp._max_timestamps(settings, TMP_ROOT, False, tstamp)
+    assert res == tstamp
+    _teardown()
+
+
+def test_get_parent_timestamp_none():
+    record_path, mtime, settings = _setup()
+    DEEP_TMP.mkdir()
+    res = timestamp._get_parent_timestamp(settings, DEEP_TMP, None)
+    assert res == mtime
+    _teardown()
+
+
+def test_get_parent_timestamp_tstamp():
+    record_path, mtime, settings = _setup()
+    DEEP_TMP.mkdir()
+    tstamp = mtime + 100
+    res = timestamp._get_parent_timestamp(settings, DEEP_TMP, tstamp)
+    assert res == tstamp
+    _teardown()
+
+
+def test_get_walk_after_none():
+    record_path, mtime, settings = _setup()
+    res = timestamp.get_walk_after(settings, TMP_ROOT, None)
+    assert res == mtime
+
+
+def test_get_walk_after_settings():
+    record_path, mtime, settings = _setup()
+    settings.optimize_after = mtime + 100
+    res = timestamp.get_walk_after(settings, TMP_ROOT, None)
+    assert res == mtime
+
+
+def test_get_walk_after_settings_tstamp():
+    record_path, mtime, settings = _setup()
+    tstamp = mtime + 100
+    res = timestamp.get_walk_after(settings, TMP_ROOT, tstamp)
+    assert res == tstamp
+
+
+def test_should_record_timestamp_unset():
+    settings = Settings()
+    res, reason = timestamp._should_record_timestamp(settings, TMP_ROOT)
+    assert not res
+    assert reason == timestamp._REASON_DEFAULT
+
+
+def test_should_record_timestamp_set_symlink():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    settings.follow_symlinks = False
+    DEEP_TMP_FILE.symlink_to(TMP_ROOT)
+    res, reason = timestamp._should_record_timestamp(settings, DEEP_TMP_FILE)
+    assert not res
+    assert reason == timestamp._REASON_SYMLINK
+    _teardown()
+
+
+def test_should_record_timestamp_set_symlink_quiet():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    settings.follow_symlinks = False
+    settings.verbose = 0
+    DEEP_TMP_FILE.symlink_to(TMP_ROOT)
+    res, reason = timestamp._should_record_timestamp(settings, DEEP_TMP_FILE)
+    assert not res
+    assert reason == timestamp._REASON_SYMLINK
+    _teardown()
+
+
+def test_should_record_timestamp_set_file():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    DEEP_TMP_FILE.touch()
+    res, reason = timestamp._should_record_timestamp(settings, DEEP_TMP_FILE)
+    assert not res
+    assert reason == timestamp._REASON_NONDIR
+    _teardown()
+
+
+def test_should_record_timestamp_set_file_quiet():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    settings.verbose = 0
+    DEEP_TMP_FILE.touch()
+    res, reason = timestamp._should_record_timestamp(settings, DEEP_TMP_FILE)
+    assert not res
+    assert reason == timestamp._REASON_NONDIR
+    _teardown()
+
+
+def test_should_record_timestamp_true():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    settings.verbose = 0
+    res, reason = timestamp._should_record_timestamp(settings, DEEP_TMP)
+    assert res
+    assert reason == timestamp._REASON_DEFAULT
+    _teardown()
+
+
+def test_remove_old_timestamp():
+    deep_record_path, _, settings = _setup(DEEP_TMP)
+    settings.record_timestamp = True
+    parent_record_path = TMP_ROOT / timestamp.RECORD_FILENAME
+    parent_record_path.touch()
+    timestamp._get_timestamp_cached(settings, DEEP_TMP, True)
+    removed = timestamp._remove_old_timestamps(settings, TMP_ROOT, parent_record_path)
+    assert not deep_record_path.exists()
+    assert len(removed) == 1
+    assert removed[deep_record_path] is None
+    _teardown()
+
+
+def test_remove_old_timestamp_quiet():
+    deep_record_path, _, settings = _setup(DEEP_TMP)
+    settings.record_timestamp = True
+    settings.verbose = 0
+    parent_record_path = TMP_ROOT / timestamp.RECORD_FILENAME
+    parent_record_path.touch()
+    timestamp._get_timestamp_cached(settings, DEEP_TMP, True)
+    removed = timestamp._remove_old_timestamps(settings, TMP_ROOT, parent_record_path)
+    assert not deep_record_path.exists()
+    assert len(removed) == 1
+    assert removed[deep_record_path] is None
+    _teardown()
+
+
+def test_remove_old_timestamp_error():
+    deep_record_path, _, settings = _setup(DEEP_TMP)
+    settings.record_timestamp = True
+    parent_record_path = TMP_ROOT / timestamp.RECORD_FILENAME
+    parent_record_path.touch()
+    timestamp._get_timestamp_cached(settings, DEEP_TMP, True)
+    deep_record_path.unlink()
+    deep_record_path.mkdir()
+    removed = timestamp._remove_old_timestamps(settings, TMP_ROOT, parent_record_path)
+    assert deep_record_path.exists()
+    assert len(removed) == 1
+    assert isinstance(removed[deep_record_path], OSError)
+    _teardown()
+
+
+def test_record_timestamp_set():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    timestamp.record_timestamp(settings, DEEP_TMP)
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    assert deep_record_path.exists()
+    _teardown()
+
+
+def test_record_timestamp_set_quiet():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    settings.verbose = 0
+    timestamp.record_timestamp(settings, DEEP_TMP)
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    assert deep_record_path.exists()
+    _teardown()
+
+
+def test_record_timestamp_set_not():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = False
+    timestamp.record_timestamp(settings, DEEP_TMP)
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    assert not deep_record_path.exists()
+    _teardown()
+
+
+def test_record_timestamp_set_not_quiet():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = False
+    settings.verbose = 0
+    timestamp.record_timestamp(settings, DEEP_TMP)
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    assert not deep_record_path.exists()
+    _teardown()
+
+
+def test_record_timestamp_set_error():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    with open(deep_record_path, "w+") as record_file:
+        fcntl.flock(record_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+        timestamp.record_timestamp(settings, DEEP_TMP)
+        fcntl.flock(record_file, fcntl.LOCK_UN)
+    assert deep_record_path.exists()
+    _teardown()
+
+
+def test_record_timestamp_set_remove():
+    record_path, settings = _setup_record()
+    settings.record_timestamp = True
+
+    timestamp.record_timestamp(settings, DEEP_TMP)
+    timestamp.record_timestamp(settings, TMP_ROOT)
+    deep_record_path = DEEP_TMP / timestamp.RECORD_FILENAME
+    assert not deep_record_path.exists()
+    _teardown()
