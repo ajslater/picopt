436c9585f0ba2e4612f80e0a99da7da9098ed7c4
AJ Slater <aj@slater.net>
2022-10-13 12:44:32 -0700

    clearer headers code for pillow extension

--- picopt/pillow/webp_lossless.py
+++ picopt/pillow/webp_lossless.py
@@ -5,25 +5,34 @@ Determine if a webp is lossless.
 This should be a part of Pillow
 https://developers.google.com/speed/webp/docs/webp_lossless_bitstream_specification
 """
+from dataclasses import dataclass
 from pathlib import Path
 
 from picopt.pillow.unpack import compare_header
 
 
-RIFF_HEADER: tuple[bytes, ...] = (b"R", b"I", b"F", b"F")
-WEBP_HEADER: tuple[bytes, ...] = (b"W", b"E", b"B", b"P")
-VP8L_HEADER: tuple[bytes, ...] = (b"V", b"P", b"8", b"L")
+@dataclass
+class ImageHeader:
+    """The seek location and value of a byte header."""
 
-COMPARATORS = ((0, RIFF_HEADER), (8, WEBP_HEADER), (12, VP8L_HEADER))
+    offset: int
+    bytes: tuple[bytes, ...]
+
+
+RIFF_HEADER = ImageHeader(0, (b"R", b"I", b"F", b"F"))
+WEBP_HEADER = ImageHeader(8, (b"W", b"E", b"B", b"P"))
+VP8L_HEADER = ImageHeader(12, (b"V", b"P", b"8", b"L"))
+
+HEADERS = (RIFF_HEADER, WEBP_HEADER, VP8L_HEADER)
 
 
 def is_lossless(filename: str) -> bool:
-    """If a file is a png, get the bit depth from the standard position."""
+    """Compare header types against lossless types."""
     path = Path(filename)
     try:
         with path.open("rb") as img:
-            for seek, byte_array in COMPARATORS:
-                compare_header(img, seek, byte_array)
+            for header in HEADERS:
+                compare_header(img, header.offset, header.bytes)
         return True
     except ValueError:
         return False
