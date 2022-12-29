fa23127724dfdecff7765c00872af704a9648789
AJ Slater <aj@slater.net>
2022-12-29 15:00:28 -0800

    update packages

--- test.sh
+++ test.sh
@@ -5,5 +5,3 @@ mkdir -p test-results
 PYTHONDONTWRITEBYTECODE=1 poetry run pytest "$@"
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 poetry run coverage erase
-poetry run vulture .
-poetry run radon mi -nc .
