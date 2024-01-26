143827f03e62ebe54e8ddc22bc227c2d47db884d
AJ Slater <aj@slater.net>
2024-01-26 12:04:07 -0800

    make webp_animated depend on png a bit

--- picopt/handlers/webp_animated.py
+++ picopt/handlers/webp_animated.py
@@ -1,11 +1,6 @@
 """WebP Animated images are treated like containers."""
 from types import MappingProxyType
 
-from picopt.formats import (
-    CONVERTIBLE_ANIMATED_FILE_FORMATS,
-    CONVERTIBLE_ANIMATED_FORMAT_STRS,
-)
-from picopt.handlers.gif import Gif, GifAnimated
 from picopt.handlers.handler import FileFormat
 from picopt.handlers.image_animated import ImageAnimated
 from picopt.handlers.png_animated import PngAnimated
@@ -24,15 +19,9 @@ class WebPAnimatedLossless(WebPAnimatedBase):
 
     OUTPUT_FILE_FORMAT = FileFormat(WebPAnimatedBase.OUTPUT_FORMAT_STR, True, True)
     INPUT_FILE_FORMATS = frozenset(
-        {
-            OUTPUT_FILE_FORMAT,
-            GifAnimated.OUTPUT_FILE_FORMAT,
-            PngAnimated.OUTPUT_FILE_FORMAT,
-        }
-        | CONVERTIBLE_ANIMATED_FILE_FORMATS,
+        {*PngAnimated.INPUT_FILE_FORMATS, OUTPUT_FILE_FORMAT}
     )
     CONVERT_FROM_FORMAT_STRS = frozenset(
-        CONVERTIBLE_ANIMATED_FORMAT_STRS
-        | {Gif.OUTPUT_FORMAT_STR, PngAnimated.OUTPUT_FORMAT_STR}
+        {*PngAnimated.CONVERT_FROM_FORMAT_STRS, PngAnimated.OUTPUT_FORMAT_STR}
     )
     PIL2_KWARGS = MappingProxyType({**WebPAnimatedBase.PIL2_KWARGS, "lossless": True})
