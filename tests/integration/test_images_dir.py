788c2c0a5347dc147eae312221ee99b33ee6fc91
AJ Slater <aj@slater.net>
2025-04-14 18:44:12 -0700

    update animated png size for linux

--- tests/integration/test_images_dir.py
+++ tests/integration/test_images_dir.py
@@ -11,7 +11,6 @@ __all__ = ()
 FNS = {
     "07themecamplist.pdf": (93676, 93676, ("pdf", 93676), ("pdf", 93676)),
     "test_animated_gif.gif": (16383, 16358, ("png", 24255), ("webp", 11892)),
-    "test_animated_png.png": (63435, 63058, ("png", 63058), ("webp", 53388)),
     "test_animated_webp.webp": (13610, 12174, ("webp", 12174), ("webp", 12174)),
     "test_png.png": (7967, 4150, ("png", 4150), ("webp", 3870)),
     "test_pre-optimized_jpg.jpg": (
@@ -39,6 +38,7 @@ FNS = {
 if platform.system() == "Darwin":
     FNS.update(
         {
+            "test_animated_png.png": (63435, 63058, ("png", 63058), ("webp", 53388)),
             "test_gif.gif": (138952, 138944, ("png", 112290), ("webp", 108058)),
             "test_pre-optimized_png.png": (
                 256572,
@@ -52,6 +52,7 @@ if platform.system() == "Darwin":
 else:
     FNS.update(
         {
+            "test_animated_png.png": (63435, 63058, ("png", 63058), ("webp", 53422)),
             "test_gif.gif": (138952, 138944, ("png", 112290), ("webp", 107952)),
             "test_pre-optimized_png.png": (
                 256572,
