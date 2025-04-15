538034fb9c0701886de7d1861046e1ab613c68e7
AJ Slater <aj@slater.net>
2025-04-14 18:38:54 -0700

    fix docker compose command

--- bin/docker-build.sh
+++ bin/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run all phases of build with docker
 set -euo pipefail
-docker-compose build picopt-build-builder
-docker-compose up --exit-code-from picopt-lint picopt-lint
-docker-compose up --exit-code-from picopt-test picopt-test
-docker-compose up --exit-code-from picopt-build picopt-build
+docker compose build picopt-build-builder
+docker compose up --exit-code-from picopt-lint picopt-lint
+docker compose up --exit-code-from picopt-test picopt-test
+docker compose up --exit-code-from picopt-build picopt-build
