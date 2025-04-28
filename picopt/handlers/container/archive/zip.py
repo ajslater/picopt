ab9ffe3e060ce6f69b5957989d95e7eb1b7b29c1
AJ Slater <aj@slater.net>
2025-04-27 23:58:43 -0700

    rename variable

--- picopt/handlers/container/archive/zip.py
+++ picopt/handlers/container/archive/zip.py
@@ -26,7 +26,7 @@ class Zip(PackingArchiveHandler):
     def __init__(self, *args, convert: bool = False, **kwargs):
         """Init delete_filenames."""
         super().__init__(*args, **kwargs)
-        self._optimize_in_place_on_disk = not convert and not self.path_info.archiveinfo
+        self._patch_zipfile_on_disk = not convert and not self.path_info.archiveinfo
         self._delete_filenames = set()
         self._bytes_in = 0
 
@@ -52,7 +52,7 @@ class Zip(PackingArchiveHandler):
         self._do_repack = True
 
     def _archive_for_write(self, output_buffer: BytesIO) -> ZipFileWithRemove:
-        if self._optimize_in_place_on_disk:
+        if self._patch_zipfile_on_disk:
             file = self.original_path
             mode = "a"
         else:
@@ -84,20 +84,24 @@ class Zip(PackingArchiveHandler):
         archive.writestr(zipinfo, data)
 
     def _archive_write(self, archive):
-        if self._optimize_in_place_on_disk:
+        if self._patch_zipfile_on_disk:
             self._delete_files_before_write(archive)
         super()._archive_write(archive)
 
     def pack_into(self) -> BytesIO:
         """Do in place deletes on disk before writing if we can."""
-        if self._optimize_in_place_on_disk:
+        if self._patch_zipfile_on_disk:
             self._bytes_in = self.path_info.bytes_in()
         return super().pack_into()
+
+    def _cleanup_filesystem_write_final_path(self, final_data_buffer: BinaryIO) -> None:
+        if not self._patch_zipfile_on_disk:
+            super()._cleanup_filesystem_write_final_path(final_data_buffer)
 
     def _cleanup_after_optimize_calculate_bytes(
         self, final_data_buffer: BinaryIO
     ) -> tuple[int, int]:
-        if self._optimize_in_place_on_disk:
+        if self._patch_zipfile_on_disk:
             bytes_in = self._bytes_in
             bytes_out = self.final_path.stat().st_size
         else:
