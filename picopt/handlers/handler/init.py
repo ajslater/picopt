18773cc21632da1e409ae5a7acff0b18117f6ba9
AJ Slater <aj@slater.net>
2025-04-29 15:08:49 -0700

    fix dots in filenames that don't indicate suffixes

--- picopt/handlers/handler/init.py
+++ picopt/handlers/handler/init.py
@@ -9,6 +9,9 @@ from picopt.formats import FileFormat
 from picopt.path import PathInfo
 from picopt.printer import Printer
 
+_BAD_SUFFIX_CHARS = " ()"
+_MAX_SUFFIX_LEN = 8
+
 
 class HandlerInit:
     """Handler init."""
@@ -22,6 +25,20 @@ class HandlerInit:
     INPUT_FILE_FORMATS: frozenset[FileFormat] = frozenset({OUTPUT_FILE_FORMAT})
     PROGRAMS: tuple[tuple[str, ...], ...] = ()
     WORKING_SUFFIX: str = f"{PROGRAM_NAME}-tmp"
+
+    def _compute_final_path(self):
+        """Compute the final path even if it the original has multiple suffixes."""
+        final_path = str(self.original_path)
+        for suffix in reversed(self.original_path.suffixes):
+            if len(suffix) > _MAX_SUFFIX_LEN or any(
+                char in suffix for char in _BAD_SUFFIX_CHARS
+            ):
+                # dots in filenames are different than multiple suffixes
+                # bail if it doesn't look like a suffix
+                break
+            final_path = final_path.removesuffix(suffix)
+        final_path += self.output_suffix
+        return Path(final_path)
 
     def __init__(
         self,
@@ -48,14 +65,7 @@ class HandlerInit:
         self.output_suffix: str = (
             suffix if (suffix.lower() in self.SUFFIXES) else default_suffix
         )
-
-        # Handle replacing multiple suffixes
-        final_path = str(self.original_path)
-        for suffix in reversed(self.original_path.suffixes):
-            final_path = final_path.removesuffix(suffix)
-        final_path = Path(final_path)
-        final_path = final_path.with_suffix(self.output_suffix)
-        self.final_path: Path = final_path
+        self.final_path: Path = self._compute_final_path()
 
         if self.config.preserve:
             self.path_info.stat()
