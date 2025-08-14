ce5a664a9207175b19aea1f6c520968698910479
AJ Slater <aj@slater.net>
2025-08-13 17:44:53 -0700

    move tests out of type dirs

--- tests/test_mpo.py
+++ tests/test_mpo.py
@@ -7,7 +7,7 @@ import pytest
 
 from picopt import PROGRAM_NAME, cli
 from tests import IMAGES_DIR, get_test_dir
-from tests.integration.base import BaseTest
+from tests.base import BaseTest
 
 __all__ = ()
 
