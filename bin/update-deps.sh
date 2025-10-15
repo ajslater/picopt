81bf34f53780fccd01eb2e50c8202c5b480dbdc1
AJ Slater <aj@slater.net>
2025-10-15 13:11:20 -0700

    update deps

--- bin/update-deps.sh
+++ bin/update-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Update python and npm dependencies
 set -euo pipefail
-uv sync --no-install-project --all-extras --upgrade
-uv lock --upgrade --dry-run
+uv sync --no-install-project --all-extras --all-groups --all-packages --upgrade
+uv tree --all-groups --depth 1 --upgrade --outdated | grep --color=always "(latest:.*)" || true
 npm update
 npm outdated
 if [ -d frontend ]; then
