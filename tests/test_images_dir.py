92790715b6e89b833dc82d4bc5b0790e88042f08
AJ Slater <aj@slater.net>
2025-12-04 12:26:37 -0800

    fix tests for jxl

--- tests/test_images_dir.py
+++ tests/test_images_dir.py
@@ -13,80 +13,207 @@ from tests.base import BaseTest
 __all__ = ()
 FNS = {
     # orig, no convert, convert, webp
-    "07themecamplist.pdf": (93676, 93676, ("pdf", 93676), ("pdf", 93676)),
-    "test_animated_webp.webp": (11856, 11856, ("webp", 11856), ("webp", 11856)),
-    "test_png.png": (7967, 4150, ("png", 4150), ("webp", 3870)),
+    "07themecamplist.pdf": (
+        93676,
+        93676,
+        ("pdf", 93676),
+        ("pdf", 93676),
+        ("pdf", 93676),
+    ),
+    "test_animated_webp.webp": (
+        11856,
+        11856,
+        ("webp", 11856),
+        ("webp", 11856),
+        ("webp", 11856),
+    ),
+    "test_png.png": (7967, 4150, ("png", 4150), ("webp", 3870), ("jxl", 3971)),
     "test_pre-optimized_jpg.jpg": (
         22664,
         22664,
         ("jpg", 22664),
         ("jpg", 22664),
+        ("jxl", 20711),
     ),
-    "test_svg.svg": (5393, 4871, ("svg", 4871), ("svg", 4871)),
-    "test_txt.txt": (6, 6, ("txt", 6), ("txt", 6)),
-    "test_webp_lossless.webp": (5334, 3870, ("webp", 3870), ("webp", 3870)),
-    "test_webp_lossy.webp": (2764, 2764, ("webp", 2764), ("webp", 2764)),
-    "test_png_16rgba.png": (3435, 2097, ("png", 2097), ("webp", 1142)),
-    "test_jpg.jpg": (97373, 87913, ("jpg", 87913), ("jpg", 87913)),
+    "test_svg.svg": (5393, 4871, ("svg", 4871), ("svg", 4871), ("svg", 4871)),
+    "test_txt.txt": (6, 6, ("txt", 6), ("txt", 6), ("txt", 6)),
+    "test_webp_lossless.webp": (
+        5334,
+        3870,
+        ("webp", 3870),
+        ("webp", 3870),
+        ("jxl", 3934),
+    ),
+    "test_webp_lossy.webp": (
+        2764,
+        2764,
+        ("webp", 2764),
+        ("webp", 2764),
+        ("webp", 2764),
+    ),
+    "test_png_16rgba.png": (3435, 2097, ("png", 2097), ("webp", 1142), ("jxl", 1053)),
+    "test_jpg.jpg": (97373, 87913, ("jpg", 87913), ("jpg", 87913), ("jxl", 16237)),
+    "test_jxl.jxl": (0, 0, ("jxl", 0), ("webp", 0), ("jxl", 77911)),
 }
 if platform.system() == "Darwin":
     FNS.update(
         {
-            "test_animated_gif.gif": (16383, 16358, ("png", 16582), ("webp", 11856)),
-            "test_animated_png.png": (63435, 61393, ("png", 61393), ("webp", 52972)),
+            "test_animated_gif.gif": (
+                16383,
+                16358,
+                ("png", 16582),
+                ("webp", 11856),
+                ("jxl", 3438),
+            ),
+            "test_animated_png.png": (
+                63435,
+                61393,
+                ("png", 61393),
+                ("webp", 52972),
+                ("jxl", 3402),
+            ),
             "test_pre-optimized_png.png": (
                 256572,
                 256572,
                 ("png", 256572),
                 ("webp", 197084),
+                ("jxl", 114836),
             ),
             "test_webp_lossless_pre-optimized.webp": (
                 3794,
                 3794,
                 ("webp", 3794),
                 ("webp", 3794),
+                ("jxl", 4172),
             ),
-            "eight.tif": (59640, 59640, ("png", 30585), ("webp", 24988)),
-            "mri.tif": (230578, 230578, ("png", 129479), ("webp", 114710)),
-            "test_bmp.bmp": (141430, 141430, ("png", 67236), ("webp", 47384)),
-            "test_pnm.pnm": (27661, 27661, ("png", 15510), ("webp", 12728)),
-            "test_gif.gif": (138952, 138944, ("png", 112290), ("webp", 107972)),
+            "eight.tif": (
+                59640,
+                59640,
+                ("png", 30585),
+                ("webp", 24988),
+                ("jxl", 21996),
+            ),
+            "mri.tif": (
+                230578,
+                230578,
+                ("png", 129479),
+                ("webp", 114710),
+                ("jxl", 4182),
+            ),
+            "test_bmp.bmp": (
+                141430,
+                141430,
+                ("png", 67236),
+                ("webp", 47384),
+                ("jxl", 41218),
+            ),
+            "test_pnm.pnm": (
+                27661,
+                27661,
+                ("png", 15510),
+                ("webp", 12728),
+                ("jxl", 12107),
+            ),
+            "test_gif.gif": (
+                138952,
+                138944,
+                ("png", 112290),
+                ("webp", 107972),
+                ("jxl", 114836),
+            ),
         }
     )
 else:
     FNS.update(
         {
-            "test_animated_gif.gif": (16383, 16358, ("png", 16582), ("webp", 11856)),
-            "test_animated_png.png": (63435, 61393, ("png", 61393), ("webp", 52972)),
+            "test_animated_gif.gif": (
+                16383,
+                16358,
+                ("png", 16582),
+                ("webp", 11856),
+                ("jxl", 3438),
+            ),
+            "test_animated_png.png": (
+                63435,
+                61393,
+                ("png", 61393),
+                ("webp", 52972),
+                ("jxl", 3402),
+            ),
             "test_pre-optimized_png.png": (
                 256572,
                 256572,
                 ("png", 256572),
                 ("webp", 197088),
+                ("jxl", 186092),
             ),
             "test_webp_lossless_pre-optimized.webp": (
                 3794,
                 3794,
                 ("webp", 3798),
                 ("webp", 3794),
+                ("jxl", 4172),
             ),
-            "eight.tif": (59640, 59640, ("png", 30585), ("webp", 25012)),
-            "mri.tif": (230578, 230578, ("png", 129479), ("webp", 116954)),
-            "test_bmp.bmp": (141430, 141430, ("png", 67236), ("webp", 47436)),
-            "test_pnm.pnm": (27661, 27661, ("png", 15510), ("webp", 12758)),
-            "test_gif.gif": (138952, 138944, ("png", 112290), ("webp", 108058)),
+            "eight.tif": (
+                59640,
+                59640,
+                ("png", 30585),
+                ("webp", 25012),
+                ("jxl", 21996),
+            ),
+            "mri.tif": (
+                230578,
+                230578,
+                ("png", 129479),
+                ("webp", 116954),
+                ("jxl", 4182),
+            ),
+            "test_bmp.bmp": (
+                141430,
+                141430,
+                ("png", 67236),
+                ("webp", 47436),
+                ("jxl", 41218),
+            ),
+            "test_pnm.pnm": (
+                27661,
+                27661,
+                ("png", 15510),
+                ("webp", 12758),
+                ("jxl", 12107),
+            ),
+            "test_gif.gif": (
+                138952,
+                138944,
+                ("png", 112290),
+                ("webp", 108058),
+                ("jxl", 114836),
+            ),
         }
     )
 
 NEAR_LOSSLESS_FNS = MappingProxyType(
     {
-        "test_png_16rgba.png": (3435, 2097, ("png", 2097), ("webp", 728)),
-        "test_webp_lossless.webp": (5334, 3870, ("webp", 3870), ("webp", 2044)),
+        "test_png_16rgba.png": (
+            3435,
+            2097,
+            ("png", 2097),
+            ("webp", 728),
+            ("jxl", 1053),
+        ),
+        "test_webp_lossless.webp": (
+            5334,
+            3870,
+            ("webp", 3870),
+            ("webp", 2044),
+            ("jxl", 3934),
+        ),
         "test_webp_lossless_pre-optimized.webp": (
             3794,
             3794,
             ("webp", 3794),
             ("webp", 3794),
+            ("jxl", 4172),
         ),
     }
 )
@@ -138,6 +265,20 @@ class TestImagesDir(BaseTest):
         path = (self.TMP_ROOT / fn).with_suffix("." + suffix)
         assert path.stat().st_size == size
 
+    def test_convert_to_jxl(self, fn: str) -> None:
+        args = (
+            PROGRAM_NAME,
+            "-rvvbx",
+            "BMP,PPM,SVG,TIFF",
+            "-c",
+            "JXL",
+            str(self.TMP_ROOT),
+        )
+        cli.main(args)
+        suffix, size = FNS[fn][4]
+        path = (self.TMP_ROOT / fn).with_suffix("." + suffix)
+        assert path.stat().st_size == size
+
 
 @pytest.mark.parametrize("fn", NEAR_LOSSLESS_FNS)
 class TestNearLosslessImageDir(BaseTest):
@@ -155,5 +296,18 @@ class TestNearLosslessImageDir(BaseTest):
         )
         cli.main(args)
         suffix, size = self.FNS[fn][3]
+        path = (self.TMP_ROOT / fn).with_suffix("." + suffix)
+        assert path.stat().st_size == size
+
+    def test_convert_to_jpegxl_near_lossless(self, fn: str) -> None:
+        """Test convert to JPEGXL."""
+        args = (
+            PROGRAM_NAME,
+            "-rvvvbnc",
+            "JXL",
+            str(self.TMP_ROOT),
+        )
+        cli.main(args)
+        suffix, size = self.FNS[fn][4]
         path = (self.TMP_ROOT / fn).with_suffix("." + suffix)
         assert path.stat().st_size == size
