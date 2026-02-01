76c2cb74f285adc04a8585a1c906a23274c6dabd
AJ Slater <aj@slater.net>
2026-02-01 01:28:51 -0800

    fix lint script with group

--- bin/lint-backend.sh
+++ bin/lint-backend.sh
@@ -7,7 +7,7 @@ set -euxo pipefail
 ####################
 uv run --group lint ruff check .
 uv run --group lint ruff format --check .
-uv run --group lint basedpyright
+uv run --group lint --group test basedpyright
 uv run --group lint vulture .
 if [ "$(uname)" = "Darwin" ]; then
   # Complexity is only of interest to development
