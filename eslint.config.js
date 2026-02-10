619729224c50d60eab212ec4d54517680e674c10
AJ Slater <aj@slater.net>
2026-02-10 13:59:57 -0800

    update deps and add eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -6,8 +6,11 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginArrayFunc from "eslint-plugin-array-func";
 import eslintPluginCompat from "eslint-plugin-compat";
+import eslintPluginDeMorgan from "eslint-plugin-de-morgan";
 import eslintPluginDepend from "eslint-plugin-depend";
+import eslintPluginHtml from "eslint-plugin-html";
 import eslintPluginImport from "eslint-plugin-import-x";
+import eslintPluginMath from "eslint-plugin-math";
 import * as eslintPluginMdx from "eslint-plugin-mdx";
 import eslintPluginNoSecrets from "eslint-plugin-no-secrets";
 import eslintPluginNoUnsanitized from "eslint-plugin-no-unsanitized";
@@ -31,8 +34,10 @@ export const CONFIGS = {
     ...eslintPluginArrayFunc.configs.all,
     ...eslintPluginComments.recommended,
     ...eslintPluginCompat.configs[FLAT_RECOMMENDED],
+    ...eslintPluginDeMorgan.configs.recommended,
     ...eslintPluginDepend.configs[FLAT_RECOMMENDED],
     ...eslintPluginImport.flatConfigs.all,
+    ...eslintPluginMath.configs.recommended,
     ...eslintPluginNoUnsanitized.configs.recommended,
     ...eslintPluginPromise.configs[FLAT_ALL],
     ...eslintPluginRegexp.configs.all,
@@ -99,6 +104,7 @@ export default defineConfig([
       "prettier/prettier": "warn",
     },
   },
+  { files: ["**/*.html"], plugins: { html: eslintPluginHtml } },
   {
     files: ["**/*.js"],
     ...CONFIGS.js,
