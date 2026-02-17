b8c0518633808ddcb7a8abd838c770ae9f66596f
AJ Slater <aj@slater.net>
2026-02-16 16:14:43 -0800

    update devenv add missing ci script

---
+++ bin/create-output-dirs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin bash
+# create output directories with correct perms for ci builder docker mounts
+set -euo pipefail
+mkdir -p -m 777 test-results dist
+chown -R circleci:circleci test-results dist
