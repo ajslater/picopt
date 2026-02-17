2a6983c0eb1613d4d3848aae2e2d27a47720a035
AJ Slater <aj@slater.net>
2026-02-16 16:27:04 -0800

    try running script with simpler sh

--- bin/create-output-dirs.sh
+++ bin/create-output-dirs.sh
@@ -1,5 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # create output directories with correct perms for ci builder docker mounts
-set -euo pipefail
 mkdir -p -m 777 test-results dist
 chown -R circleci:circleci test-results dist
