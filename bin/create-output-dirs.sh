739d518cb861b2b22f103978200f971dace866a1
AJ Slater <aj@slater.net>
2026-02-16 16:35:58 -0800

    add debug command for circlecir persist

--- bin/create-output-dirs.sh
+++ bin/create-output-dirs.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # create output directories with correct perms for ci builder docker mounts
+set -euo pipefail
 mkdir -p -m 777 test-results dist
 chown -R circleci:circleci test-results dist
